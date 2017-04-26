a32ad3c915c5fff6e27169085b4db3af6f78d304
Hidehiro Nagaoka <kuinige@gmail.com>
2017-04-26 09:19:33 +0900

    added product listing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Creating products...'
+5.times do
+  Product.create!(
+    name: Faker::Company.name,
+    tagline: Faker::Company.catch_phrase
+  )
+end
+puts 'Finished!'
