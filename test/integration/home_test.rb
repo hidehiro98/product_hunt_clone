943764bb4aa5d82dd1132b1b00b1d24c34c52d12
Hidehiro Nagaoka <kuinige@gmail.com>
2017-04-26 10:02:04 +0900

    add test for product listing

---
+++ test/integration/home_test.rb
@@ -0,0 +1,24 @@
+require "test_helper"
+
+class HomeTest < ActionDispatch::IntegrationTest
+  test "loads correctly" do
+
+    # setup
+    # no
+
+    # exercise
+    visit "/"
+
+    save_and_open_screenshot
+    # save_and_open_page
+    # puts page.html
+
+    # verify
+    assert_equal 200, page.status_code
+    assert page.has_content?("Products")
+    assert page.has_selector?('.product', count: Product.count)
+    # assert page.has_selector?('.product', count: 42)
+
+    # teardown
+  end
+end
